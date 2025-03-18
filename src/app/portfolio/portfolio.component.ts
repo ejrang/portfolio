0f29f08e66be01c6457b0a64f7207935e6c5415b
Ejran <ejrannahimzadah@gmail.com>
2025-03-18 19:25:54 +0100

    first commit

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { IntroductionSectionComponent } from '../introduction-section/introduction-section.component';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
 import { WhyMeSectionComponent } from '../why-me-section/why-me-section.component';
 import { MySkillsSectionComponent } from '../my-skills-section/my-skills-section.component';
 import { MyProjectsSectionComponent } from '../my-projects-section/my-projects-section.component';
@@ -8,10 +8,12 @@ import { QuotesSectionComponent } from '../quotes-section/quotes-section.compone
 import { ContactMeSectionComponent } from '../contact-me-section/contact-me-section.component';
 import { FooterComponent } from '../footer/footer.component';
 
+
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [HeaderComponent, IntroductionSectionComponent,
+  imports: [HeaderComponent, 
+     HeroSectionComponent,
      WhyMeSectionComponent, 
      MySkillsSectionComponent,
      MyProjectsSectionComponent,
